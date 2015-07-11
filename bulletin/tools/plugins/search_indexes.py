5c085729c09efff40941ff7cd452f436a3383872
rerb <bob.erb@aashe.org>
2015-07-10 20:49:23 -0700

    Exclude un-approved content from search results.

--- bulletin/tools/plugins/search_indexes.py
+++ bulletin/tools/plugins/search_indexes.py
@@ -2,53 +2,56 @@ from haystack import indexes
 import models
 
 
-class ApprovedIndexMixin(object):
-
-
-    def index_queryset(self, **kwargs):
-        return self.get_model().objects.filter(approved=True)
-
-
 class EventIndex(indexes.SearchIndex,
-                 indexes.Indexable,
-                 ApprovedIndexMixin):
+                 indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
 
     def get_model(self):
         return models.Event
 
+    def index_queryset(self, **kwargs):
+        return models.Event.objects.filter(approved=True)
+
 
 class JobIndex(indexes.SearchIndex,
-               indexes.Indexable,
-               ApprovedIndexMixin):
+               indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
 
     def get_model(self):
         return models.Job
 
+    def index_queryset(self, **kwargs):
+        return models.Job.objects.filter(approved=True)
+
 
 class NewResourceIndex(indexes.SearchIndex,
-                       indexes.Indexable,
-                       ApprovedIndexMixin):
+                       indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
 
     def get_model(self):
         return models.NewResource
 
+    def index_queryset(self, **kwargs):
+        return models.NewResource.objects.filter(approved=True)
+
 
 class OpportunityIndex(indexes.SearchIndex,
-                       indexes.Indexable,
-                       ApprovedIndexMixin):
+                       indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
 
     def get_model(self):
         return models.Opportunity
 
+    def index_queryset(self, **kwargs):
+        return models.Opportunity.objects.filter(approved=True)
+
 
 class StoryIndex(indexes.SearchIndex,
-                 indexes.Indexable,
-                 ApprovedIndexMixin):
+                 indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
 
     def get_model(self):
         return models.Story
+
+    def index_queryset(self, **kwargs):
+        return models.Story.objects.filter(approved=True)
