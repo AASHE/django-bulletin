85e3f702022efd6bebca46bf82caf47679c86c24
rerb <bob.erb@aashe.org>
2016-02-01 12:01:50 -0500

    Bump version to 0.1.2
    Version probably should be up at or over 1.0 by now.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='django-bulletin',
-      version='0.1.1',
+      version='0.1.2',
       description="A simple newsletter application.",
       author='Bob Erb',
       author_email='bob.erb@aashe.org',
