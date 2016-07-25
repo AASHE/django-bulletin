f19622e5857f0642ee97ec31c36d07c844a619fb
rerb <bob.erb@aashe.org>
2016-07-25 12:02:00 -0400

    Add package "bulletin.tools".

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='django-bulletin',
-      version='2.2.2',
+      version='2.2.3',
       description="A simple newsletter application.",
       author='Bob Erb',
       author_email='bob.erb@aashe.org',
@@ -16,7 +16,8 @@ setup(name='django-bulletin',
       long_description=read("README.rst"),
       packages=[
           'bulletin',
-          'bulletin.migrations'
+          'bulletin.migrations',
+          'bulletin.tools'
       ],
       classifiers=[
           'Environment :: Web Environment',
