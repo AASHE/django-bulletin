2db2b42faabed4caa58d56278de6bcf8f34fd3d4
Robert Erb <rerb@mbp.local>
2018-09-11 22:40:36 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 
 setup(name='django-bulletin',
-      version='3.4.4',
+      version='3.4.5',
       description="A simple newsletter application.",
       author='Bob Erb',
       author_email='bob.erb@aashe.org',
@@ -32,7 +32,7 @@ setup(name='django-bulletin',
           "Django==1.9.13",
           "Pillow<5",
           "django-bootstrap-pagination==1.5.0",
-          "django-bootstrap3==4.11.0",
+          "django-bootstrap3==7.0.0",
           "django-braces==1.4.0",
           "django-constant-contact==1.4.1",
           "django-cors-headers==0.13",
